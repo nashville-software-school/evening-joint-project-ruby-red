2a086010a833f32cd045f90569e756de17bd56e7
Martin Cross <mncross@gmail.com>
2015-10-11 16:51:11 -0500

    added haunt incrementer

--- javascripts/haunt.js
+++ javascripts/haunt.js
@@ -4,7 +4,7 @@ define(["jquery"], function($) {
 		like: function(firebaseRef, userKey) {
 			$.ajax("https://monster-dating.firebaseio.com/users.json").done(function(users) {
       	console.log("hauntCount", users[userKey].hauntCount);
-
+      	users[userKey].hauntCount++;
       });
 		}
 	};
